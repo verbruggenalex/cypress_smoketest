da4440041ca092fdde064be632d323364f030c05
Alex Verbruggen <verbruggenalex@gmail.com>
2021-05-14 10:26:07 +0200

    Notes.

--- cypress/integration/smoketest_spec.js
+++ cypress/integration/smoketest_spec.js
@@ -1,8 +1,13 @@
-describe('My First Test', () => {
-  it('Visits the Kitchen Sink', () => {
+describe('Smoketest', () => {
+  it('Loop over retrieved routes.', () => {
+    // Visit autologin endpoint and retrieve routes.
     cy.request('http://web/cypress_smoketest/login/administrator')
       .then((response) => {
         let urls = Object.values(response.body.data);
+        // This loop should be put in its own it() function so we
+        // Get better reporting of what url passed/failed.
+        // @see: https://docs.cypress.io/guides/core-concepts/writing-and-organizing-tests#Dynamically-Generate-Tests
+        // @note: it() can not be nested :(
         urls.forEach(function (url) {
           cy.visit(url)
         });
