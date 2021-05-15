0740e709fb64eb728f2564f06d7494759e403f89
Alex Verbruggen <verbruggenalex@gmail.com>
2021-05-15 23:57:03 +0200

    Ignore baseline.

--- applitools.config.js
+++ applitools.config.js
@@ -2,11 +2,13 @@ String apiKey = System.getenv("APPLITOOLS_API_KEY");
 eyes.setApiKey(apiKey);
 
 module.exports = {
+  ignoreBaseline: true,
   testConcurrency: 1,
   apiKey: apiKey,
   browser: [
     // Add browsers with different viewports
-    {width: 1024, height: 768, name: 'chrome'}
+    {width: 1024, height: 768, name: 'chrome'},
+    {width: 1024, height: 768, name: 'edgechromium'},
   ],
   // set batch name to the configuration
   batchName: 'Ultrafast Batch'
