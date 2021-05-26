e963a20017ecee1285663affae91d69d6738124e
Alexander Verbruggen <alexander.verbruggen@intracto.com>
2021-05-26 20:41:03 +0200

    Add hook init to try and trigger an error on a page.

--- src/Controller/CypressSmoketestController.php
+++ src/Controller/CypressSmoketestController.php
@@ -6,6 +6,7 @@ use Drupal\Core\Menu\MenuTreeParameters;
 use Drupal\user\Entity\User;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Url;
+use Drupal\Component\Render\FormattableMarkup;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
@@ -32,10 +33,13 @@ class CypressSmoketestController extends ControllerBase {
   }
 
   /**
-   * Login.
+   * Login endpoint to create and login user as role.
+   * @param $role
    *
-   * @return string
-   *   Return Hello string.
+   * @return array|\Symfony\Component\HttpFoundation\RedirectResponse
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   * @throws \Drupal\Core\Entity\EntityStorageException
    */
   public function login($role) {
     $roles = $this->entityTypeManager()->getStorage('user_role')->loadMultiple();
@@ -109,66 +113,68 @@ class CypressSmoketestController extends ControllerBase {
     }
   }
 
+  /**
+   * Get watchdog messages between two timestamps.
+   *
+   * @todo: apparently timestamps on JS and PHP are different. Needs to be resolved.
+   *
+   * @param int $test_start_timestamp
+   * @param int $test_end_timestamp
+   *
+   * @return \Symfony\Component\HttpFoundation\JsonResponse
+   */
   public function getWatchdogPhpMessages(int $test_start_timestamp, int $test_end_timestamp) {
+    $errors = [];
     $result = \Drupal::database()->select('watchdog', 'w')
       ->fields('w', [])
       ->condition('w.type', 'php', '=')
       ->condition('w.timestamp', [$test_start_timestamp, $test_end_timestamp], 'BETWEEN')
       ->execute();
-    if (empty($result)) {
-      return [
-        '#type' => 'markup',
-        '#markup' => $this->t("No php messages logged between $test_start_timestamp and $test_end_timestamp."),
-      ];
+    foreach ($result as $entry) {
+      $messagePlaceholders = unserialize($entry->variables);
+      $message = new FormattableMarkup($entry->message, $messagePlaceholders);
+      $errors[] = strip_tags($message);
     }
-    else {
-      $errors = [];
-      foreach ($result as $entry) {
-        $messagePlaceholders = unserialize($entry->variables);
-        $message = new FormattableMarkup($entry->message, $messagePlaceholders);
-        $errors[] = strip_tags($message);
-      }
-      return new JsonResponse(json_encode($errors));
-    }
+    return new JsonResponse(json_encode($errors));
   }
 
   /**
+   * Helper function to return all admin menu links for current user.
    *
+   * @return \Symfony\Component\HttpFoundation\JsonResponse
    */
   public function getAdminMenuLinksForCurrentUser() {
-    $roles = $this->currentUser()->getRoles();
-    $all_links = [];
-    foreach ($roles as $role) {
-      // Get all links from the toolbar.
-      $menu_tree = \Drupal::service('toolbar.menu_tree');
-      $parameters = new MenuTreeParameters();
-      $parameters->setMinDepth(2)->setMaxDepth(5);
-      $tree = $menu_tree->load('admin', $parameters);
-      $manipulators = [
-        ['callable' => 'menu.default_tree_manipulators:checkAccess'],
-        ['callable' => 'menu.default_tree_manipulators:generateIndexAndSort'],
-        ['callable' => 'toolbar_menu_navigation_links'],
-      ];
-      $tree = $menu_tree->transform($tree, $manipulators);
-      $links = $this->retrieveLinks($tree);
-      // Remove some things we don't really want or fail.
-      $links = array_filter($links, function ($link) {
-        return (stripos($link, '?') === FALSE);
-      });
-      $links = array_diff($links, [
-        '/update.php',
-        '/user/logout',
-        '/admin/structure/contact/manage/personal',
-        '/admin/structure/contact/manage/personal/delete',
-      ]);
-
-      $all_links += $links;
-    }
-    return new JsonResponse($all_links);
+    // Get all links from the toolbar.
+    $menu_tree = \Drupal::service('toolbar.menu_tree');
+    $parameters = new MenuTreeParameters();
+    $parameters->setMinDepth(2)->setMaxDepth(5);
+    $tree = $menu_tree->load('admin', $parameters);
+    $manipulators = [
+      ['callable' => 'menu.default_tree_manipulators:checkAccess'],
+      ['callable' => 'menu.default_tree_manipulators:generateIndexAndSort'],
+      ['callable' => 'toolbar_menu_navigation_links'],
+    ];
+    $tree = $menu_tree->transform($tree, $manipulators);
+    $links = $this->retrieveLinks($tree);
+    // Remove some things we don't really want or fail.
+    $links = array_filter($links, function ($link) {
+      return (stripos($link, '?') === FALSE);
+    });
+    $links = array_diff($links, [
+      '/update.php',
+      '/user/logout',
+      '/admin/structure/contact/manage/personal',
+      '/admin/structure/contact/manage/personal/delete',
+    ]);
+    return new JsonResponse($links);
   }
 
   /**
+   * Get menu links out of tree.
    *
+   * @param $tree
+   *
+   * @return array
    */
   public function retrieveLinks($tree) {
     $links = [];
